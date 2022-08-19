5bf00b96ede3998a8ef4c6cf9581d5b1e5dc9f89
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2022-08-20 00:34:56 +0530

    commit

--- src/problems/tree/MaximumPathSumBinaryTree.java
+++ src/problems/tree/MaximumPathSumBinaryTree.java
@@ -2,46 +2,48 @@ package problems.tree;
 
 public class MaximumPathSumBinaryTree {
 
-    public static void main(String args[]){
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.left = new TreeNode(-2);
-        root.left.right = new TreeNode(-1);
-        System.out.println(maxPathSum(root));
-    }
-    
-    static class Holder{
-        public int value = Integer.MIN_VALUE;
-    }
-    public static int maxPathSum(TreeNode root) {
-        Holder holder = new Holder();
-        value(root,holder);
-        return holder.value;
-    }
-    private static int value(TreeNode root,Holder holder){
-        if(null==root){
-            return 0;
-        }
-        int leftValue = value(root.left,holder);
-        int rightValue =value(root.right,holder);
-        
-        //if its a part of ans
-        int maxValue = Math.max(leftValue,rightValue);
-        int value = maxValue>0 ? root.val + maxValue : root.val;
-        
-        //if its the ans
-        int tempPathSum = root.val;
-        if(leftValue>0){
-            tempPathSum=tempPathSum+leftValue;
-        }
-        if(rightValue>0){
-            tempPathSum=tempPathSum+rightValue;
-        }
-        if(tempPathSum>holder.value){
-            holder.value = tempPathSum;
-        }
-        
-        return value;
-    }
+	public static void main(String args[]) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(-2);
+		root.left.right = new TreeNode(-1);
+		System.out.println(maxPathSum(root));
+	}
+
+	static class Holder {
+		public int value = Integer.MIN_VALUE;
+	}
+
+	public static int maxPathSum(TreeNode root) {
+		Holder holder = new Holder();
+		value(root, holder);
+		return holder.value;
+	}
+
+	private static int value(TreeNode root, Holder holder) {
+		if (null == root) {
+			return 0;
+		}
+		int leftValue = value(root.left, holder);
+		int rightValue = value(root.right, holder);
+
+		// if its a part of ans
+		int maxValue = Math.max(leftValue, rightValue);
+		int value = maxValue > 0 ? root.val + maxValue : root.val;
+
+		// if its the ans
+		int tempPathSum = root.val;
+		if (leftValue > 0) {
+			tempPathSum = tempPathSum + leftValue;
+		}
+		if (rightValue > 0) {
+			tempPathSum = tempPathSum + rightValue;
+		}
+		if (tempPathSum > holder.value) {
+			holder.value = tempPathSum;
+		}
+
+		return value;
+	}
 }
--- src/problems/tree/MaximumPathSumOnlyLeafNodesBinaryTree.java
+++ src/problems/tree/MaximumPathSumOnlyLeafNodesBinaryTree.java
@@ -2,41 +2,41 @@ package problems.tree;
 
 public class MaximumPathSumOnlyLeafNodesBinaryTree {
 
-    public static void main(String args[]) {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.left = new TreeNode(-2);
-        root.left.right = new TreeNode(-1);
-        System.out.println(maxPathSum(root));
-    }
+	public static void main(String args[]) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(-2);
+		root.left.right = new TreeNode(-1);
+		System.out.println(maxPathSum(root));
+	}
 
-    static class Holder {
-        public int value = Integer.MIN_VALUE;
-    }
+	static class Holder {
+		public int value = Integer.MIN_VALUE;
+	}
 
-    public static int maxPathSum(TreeNode root) {
-        Holder holder = new Holder();
-        value(root, holder);
-        return holder.value;
-    }
+	public static int maxPathSum(TreeNode root) {
+		Holder holder = new Holder();
+		value(root, holder);
+		return holder.value;
+	}
 
-    private static int value(TreeNode root, Holder holder) {
-        if (null == root) {
-            return 0;
-        }
-        int leftValue = value(root.left, holder);
-        int rightValue = value(root.right, holder);
+	private static int value(TreeNode root, Holder holder) {
+		if (null == root) {
+			return 0;
+		}
+		int leftValue = value(root.left, holder);
+		int rightValue = value(root.right, holder);
 
-        // if its a part of ans
-        int value = root.val + Math.max(leftValue, rightValue);
+		// if its a part of ans
+		int value = root.val + Math.max(leftValue, rightValue);
 
-        // if its the ans
-        int tempPathSum = root.val + leftValue + rightValue;
-        if (tempPathSum > holder.value) {
-            holder.value = tempPathSum;
-        }
+		// if its the ans
+		int tempPathSum = root.val + leftValue + rightValue;
+		if (tempPathSum > holder.value) {
+			holder.value = tempPathSum;
+		}
 
-        return value;
-    }
+		return value;
+	}
 }
