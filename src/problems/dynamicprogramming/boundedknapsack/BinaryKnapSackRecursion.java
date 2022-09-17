559346a93c59ff800292a3a58987064e0e5dd941
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2022-09-17 12:16:09 +0530

    commit

--- src/problems/dynamicprogramming/boundedknapsack/BinaryKnapSackRecursion.java
+++ src/problems/dynamicprogramming/boundedknapsack/BinaryKnapSackRecursion.java
@@ -1,24 +1,29 @@
 package problems.dynamicprogramming.boundedknapsack;
 
-public class BinaryKnapSackRecursion{
-   public static void main(String args[]){
-    int wt[] = {1,3,4,5,9,12};
-    int val[]={1,4,5,7,12,10};
-    int n= val.length;
-    int w=10;
-    // Long start = System.nanoTime();
-    System.out.println(knapsack(val, wt, w, n));
-    // Long end = System.nanoTime();
-    // System.out.println(end-start);
-   } 
-   public static int knapsack(int[] val,int[] wt,int w,int n){
-       if(w==0 || n==0){
-           return 0;
-       }
-       if(wt[n-1]<=w){
-        return  Math.max(val[n-1] + knapsack(val, wt, w-wt[n-1], n-1), knapsack(val, wt, w, n-1));
-       }else{
-        return knapsack(val, wt, w, n-1);
-       }
-   }
+public class BinaryKnapSackRecursion {
+	public static void main(String args[]) {
+		type1();
+	}
+
+	private static void type1() {
+		int wt[] = { 1, 3, 4, 5, 9, 12 };
+		int val[] = { 1, 4, 5, 7, 12, 10 };
+		int n = val.length;
+		int w = 10;
+		// Long start = System.nanoTime();
+		System.out.println(knapsack(val, wt, w, n));
+		// Long end = System.nanoTime();
+		// System.out.println(end-start);
+	}
+
+	public static int knapsack(int[] val, int[] wt, int w, int n) {
+		if (w == 0 || n == 0) {
+			return 0;
+		}
+		if (wt[n - 1] <= w) {
+			return Math.max(val[n - 1] + knapsack(val, wt, w - wt[n - 1], n - 1), knapsack(val, wt, w, n - 1));
+		} else {
+			return knapsack(val, wt, w, n - 1);
+		}
+	}
 }
---
+++ src/problems/recursion/CombinationSumWithUniqueElements.java
@@ -0,0 +1,50 @@
+package problems.recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Problem links:
+ * https://leetcode.com/problems/combination-sum-ii/
+ * https://www.codingninjas.com/codestudio/problems/1112622?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+ * 
+ * Solution link
+ * https://www.youtube.com/watch?v=G1fRTGRxXU8&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=51
+ * 
+ * */
+public class CombinationSumWithUniqueElements {
+	/*
+	 * Given a collection of candidate numbers (candidates) and a target number
+	 * (target), find all unique combinations in candidates where the candidate
+	 * numbers sum to target. Each number in candidates may only be used once in the
+	 * combination. Note: The solution set must not contain duplicate combinations.
+	 */
+	public static void main(String[] args) {
+		int[] candidates = { 10, 1, 2, 7, 6, 1, 5 };
+		int target = 8;
+		Arrays.sort(candidates);
+		List<List<Integer>> answer = new ArrayList<>();
+		List<Integer> bucket = new ArrayList<>();
+		traverse(candidates, 0, target, bucket, answer);
+		System.out.println(answer);
+	}
+
+	private static void traverse(int[] candidates, int currentIndex, int target, List<Integer> bucket,
+			List<List<Integer>> answer) {
+		if (target == 0) {
+			answer.add(new ArrayList<>(bucket));
+			return;
+		}
+		for (int i = currentIndex; i < candidates.length; i++) {
+			if (i != currentIndex && candidates[i] == candidates[i - 1])
+				continue;
+			if (candidates[i] > target)
+				return;
+			bucket.add(candidates[i]);
+			traverse(candidates, i + 1, target - candidates[i], bucket, answer);
+			bucket.remove(bucket.size() - 1);
+		}
+	}
+
+}
