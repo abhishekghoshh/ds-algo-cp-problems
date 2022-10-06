12569f97fc808aec2a15c8719a949c76551594a9
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2022-10-06 23:26:34 +0530

    commit

--- src/problems/binarysearch/MatrixMedian.java
+++ src/problems/binarysearch/MatrixMedian.java
@@ -12,20 +12,70 @@ import java.util.List;
  * Solution is :
  * https://www.youtube.com/watch?v=63fPPOdIr2c&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=63
  * 
+ * https://www.youtube.com/watch?v=_4rxBuhyLXw
+ * https://www.geeksforgeeks.org/find-median-row-wise-sorted-matrix/#:~:text=Simple%20Method%3A%20The%20simplest%20method,O(r*c).
  * */
 public class MatrixMedian {
 
 	public static void main(String[] args) {
 		type1();
 		type2();
+//		System.out.println(search(new int[] { 1, 3, 6, 6, 6, 7, 8 }, 1));
 	}
 
+	// TODO check the
+	// https://www.geeksforgeeks.org/find-median-row-wise-sorted-matrix/#:~:text=Simple%20Method%3A%20The%20simplest%20method,O(r*c).
+	// url later and study one more time
 	private static void type2() {
-
+		int[][] matrix = { { 1, 3, 5 }, { 2, 6, 9 }, { 3, 6, 9 } };
+		int r = matrix.length, c = matrix[0].length;
+		int low = Integer.MAX_VALUE, high = Integer.MAX_VALUE;
+		for (int i = 0; i < r; i++) {
+			if (low > matrix[i][0]) {
+				low = matrix[i][0];
+			}
+			if (high < matrix[i][c - 1]) {
+				high = matrix[i][c - 1];
+			}
+		}
+		//
+		int medianIndex = (r * c + 1) / 2;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			int position = 0;
+			for (int i = 0; i < r; i++) {
+				position = position + upperBoundIndex(matrix[i], mid);
+			}
+			if (position < medianIndex) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		System.out.println(low);
 	}
 
+	private static int upperBoundIndex(int[] row, int item) {
+		int low = 0, high = row.length - 1, mid;
+		while (low <= high) {
+			mid = low + (high - low) / 2;
+			if (row[mid] < item) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return low;
+	}
+
+	// brute force approach
+	// if r is row and c is column and n=r*c
+	// then time complexity is O(n+n*log(n))
+	// O(n) to put it in list
+	// O(n*log(n)) to sort the list
+	// space complexity O(n) for the list
 	private static void type1() {
-		int[][] matrix = { { 1, 3, 6 }, { 2, 6, 9 }, { 3, 6, 9 } };
+		int[][] matrix = { { 1, 3, 5 }, { 2, 6, 9 }, { 3, 6, 9 } };
 		List<Integer> list = new ArrayList<>();
 		for (int[] row : matrix) {
 			for (int item : row) {
@@ -33,10 +83,11 @@ public class MatrixMedian {
 			}
 		}
 		Collections.sort(list);
+		System.out.println(list);
 		int size = list.size();
 		if (size % 2 == 0) {
 			System.out.println((list.get(size / 2 - 1) + list.get(size / 2)) / 2);
-		} else { 
+		} else {
 			System.out.println(list.get(size / 2));
 		}
 	}
--- src/problems/binarysearch/MinimumNumberOfPages.java
+++ src/problems/binarysearch/MinimumNumberOfPages.java
@@ -24,6 +24,7 @@ public class MinimumNumberOfPages {
 				8, 44, 68, 90, 24);
 		int B = 26;
 		if (B > A.size())
+			// we don't have enough book to give one book to every person
 			return;
 		// at max one can take all the books so high will be sum of pages of all books
 		// at min one can take only one book but if anyone is taking one book then also
@@ -41,19 +42,31 @@ public class MinimumNumberOfPages {
 			// we are checking that if mid number of pages can be allocated to all the
 			// students or not, if possible then we are reducing our search space, but mid
 			// is also one answer
-			if (isValid(A, B, mid)) {
+			int isValid = isValid(A, B, mid);
+			if (isValid == 0) {
 				answer = mid;
 				high = mid - 1;
+			} else if (isValid == 1) {
+				high = mid - 1;
 			} else {
-				// if mid number pages can not be allocated then we have to increase the umber
+				// if mid number pages can not be allocated then we have to increase the number
 				// of pages, so we are changing our low to mid+1
 				low = mid + 1;
 			}
 		}
+		// the answer is -1 means we can not allocate low number of pages to everyone
+		// but we have more books than students so we can allocate one book to every
+		// person, so can give books to every person at least but the sum of the pages
+		// of any individual will not exceed 97, otherwise answer will not be -1
+		// we can give the book with 97 pages to anyone and rest of the books to give
+		// simultaneously, so the maximum number of pages,that can be allocated will be
+		// 97 (highest number of books or low variable)
+		if (answer == -1)
+			answer = low;
 		System.out.println(answer);
 	}
 
-	private static boolean isValid(List<Integer> A, int students, int maxPages) {
+	private static int isValid(List<Integer> A, int students, int maxPages) {
 		// we are initializing our students as 1 as we are starting with our first
 		// student
 		int studentsCalculated = 1;
@@ -64,14 +77,18 @@ public class MinimumNumberOfPages {
 				sum = sum + A.get(i);
 			} else {
 				// the student is unable to take the book
-				// so we are giving the book t
+				// so we are giving the book to a new student
 				sum = A.get(i);
 				studentsCalculated++;
 			}
 		}
-		// this should be studentsCalculated == students
-		// as all students have to have atleast one book
-		return studentsCalculated <= students;
+		if (studentsCalculated == students) {
+			return 0;
+		} else if (studentsCalculated < students) {
+			return 1;
+		} else {
+			return -1;
+		}
 	}
 
 }
