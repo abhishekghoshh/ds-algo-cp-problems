5662aa0d7c9a3813afdda24075ef16459426eeb3
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2022-10-15 16:17:27 +0530

    commit

---
+++ src/util/ArrayBuilder.java
@@ -0,0 +1,14 @@
+package util;
+
+public class ArrayBuilder {
+	public static int[] buildArrayFromTestString(String str) {
+		String[] arr = str.trim().split(" ");
+		int[] nums = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			nums[i] = Integer.parseInt(arr[i]);
+		}
+		System.gc();
+//		Runtime.getRuntime().gc();
+		return nums;
+	}
+}
