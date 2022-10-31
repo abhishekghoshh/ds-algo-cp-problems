9b2463deb4fa974f9d213212bcaadfc9903e991e
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2022-10-31 22:08:50 +0530

    commit

--- src/problems/dynamicprogramming/longestcommonsubsequence/LongestRepeatingSubsequence.java
+++ src/problems/dynamicprogramming/longestcommonsubsequence/LongestRepeatingSubsequence.java
@@ -1,37 +1,63 @@
 package problems.dynamicprogramming.longestcommonsubsequence;
+/*
+ * Problem link :
+ * https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1
+ * 
+ * Solution link :
+ * https://www.youtube.com/watch?v=hbTaCmQGqLg&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=29
+ * 
+ */
 
 public class LongestRepeatingSubsequence {
-    public static void main(String[] args){
-        String firstString = "abbccabd";
-        String secondString = firstString;
-        int length1 = firstString.length();
-        int length2 = secondString.length();
-        int[][] memo = initialize(length1, length2);
-        int count = longestCommonSubsequence(firstString,secondString,length1,length2,memo);
-        System.out.println(count);
-    }
+	public static void main(String[] args) {
+		type3();
+		type4();
+	}
 
-    private static int[][] initialize(int length1, int length2) {
-        int[][] memo = new int[length1+1][length2+1];
-        for(int i=0;i<=length1;i++){
-            memo[i][length2]=0;
-        }
-        for(int j=0;j<=length2;j++){
-            memo[length1][j]=0;
-        }
-        return memo;
-    }
+	// same as type3 but little optimized
+	private static void type4() {
+		String str = "abbccabd";
+		char[] arr = str.toCharArray();
+		int n = arr.length;
+		int[][] memo = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (arr[i - 1] == arr[j - 1] && i != j) {
+					memo[i][j] = 1 + memo[i - 1][j - 1];
+				} else {
+					memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+				}
+			}
+		}
+		int count = memo[n][n];
+		System.out.println(count);
+	}
 
-    private static int longestCommonSubsequence(String firstString, String secondString, int length1, int length2, int[][] memo) {
-        for(int i=1;i<=length1;i++){
-            for(int j=1;j<=length2;j++){
-                if(firstString.charAt(i-1)==secondString.charAt(j-1) && i!=j){
-                    memo[i][j]=1+memo[i-1][j-1];
-                }else{
-                    memo[i][j] = Math.max(memo[i-1][j], memo[i][j-1]);
-                }
-            }
-        }
-        return memo[length1][length2];
-    }
+	private static void type3() {
+		String str = "abbccabd";
+		char[] arr1 = str.toCharArray();
+		int n = arr1.length;
+		char[] arr2 = new char[n];
+		for (int i = 0; i < n; i++) {
+			arr2[i] = arr1[i];
+		}
+		int[][] memo = new int[n + 1][n + 1];
+		for (int i = 0; i <= n; i++) {
+			memo[i][n] = 0;
+		}
+		for (int j = 0; j <= n; j++) {
+			memo[n][j] = 0;
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (arr1[i - 1] == arr2[j - 1] && i != j) {
+					memo[i][j] = 1 + memo[i - 1][j - 1];
+				} else {
+					memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+				}
+			}
+		}
+		int count = memo[n][n];
+		System.out.println(count);
+	}
 }
--- src/problems/dynamicprogramming/longestcommonsubsequence/SequencePatternMatching.java
+++ src/problems/dynamicprogramming/longestcommonsubsequence/SequencePatternMatching.java
@@ -1,37 +1,52 @@
 package problems.dynamicprogramming.longestcommonsubsequence;
+/*
+ * Problem link :
+ * 
+ * 
+ * Solution link :
+ * https://www.youtube.com/watch?v=QVntmksK2es&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=30
+ * 
+ */
 
 public class SequencePatternMatching {
-    public static void main(String[] args){
-        String str="abcdefgh";
-        String substring ="bcfgh";
-        boolean isPossible= false;
-        isPossible=sequencePatternMatchingDP(str,substring,str.length(),substring.length());
-        System.out.println(isPossible);
-        isPossible=sequencePatternMatching(str,substring,str.length(),substring.length());
-        System.out.println(isPossible);
-    }
+	public static void main(String[] args) {
+		type2();
+		type3();
+	}
 
-    private static boolean sequencePatternMatching(String str, String substring, int length, int length2) {
-        int index=0;
-        for(int i=0;i<length;i++){
-            if(str.charAt(i)==substring.charAt(index)){
-                index++;
-            }
-        }
-        return index==length2;
-    }
+	private static void type3() {
+		String s1 = "abcdefgh";
+		String s2 = "bcfgh";
+		int n1 = s1.length();
+		int n2 = s2.length();
+		int j = 0;
+		for (int i = 0; i < n1; i++) {
+			if (s1.charAt(i) == s2.charAt(j)) {
+				j++;
+			}
+		}
+		boolean isPossible = j == n2;
+		System.out.println(isPossible);
+	}
 
-    private static boolean sequencePatternMatchingDP(String str, String substring, int length, int length2) {
-        int[][] memo= new int[length+1][length2+1];
-        for(int i=1;i<=length;i++){
-            for(int j=1;j<=length2;j++){
-                if(str.charAt(i-1)==substring.charAt(j-1)){
-                    memo[i][j]=1+memo[i-1][j-1];
-                }else{
-                    memo[i][j]=Math.max(memo[i-1][j], memo[i][j-1]);
-                }
-            }
-        }
-        return memo[length][length2]==length2; 
-    }
+	private static void type2() {
+		String s1 = "abcdefgh";
+		String s2 = "bcfgh";
+		boolean isPossible = false;
+		int n1 = s1.length();
+		int n2 = s2.length();
+		System.out.println(isPossible);
+		int[][] memo = new int[n1 + 1][n2 + 1];
+		for (int i = 1; i <= n1; i++) {
+			for (int j = 1; j <= n2; j++) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+					memo[i][j] = 1 + memo[i - 1][j - 1];
+				} else {
+					memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+				}
+			}
+		}
+		isPossible = memo[n1][n2] == n2;
+		System.out.println(isPossible);
+	}
 }
