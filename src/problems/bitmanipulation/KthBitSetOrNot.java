e5fc787978222c8c12376c6ef45d67257d24b216
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2022-11-04 11:19:45 +0530

    commit

--- src/problems/bitmanipulation/KthBitSetOrNot.java
+++ src/problems/bitmanipulation/KthBitSetOrNot.java
@@ -8,7 +8,7 @@ package problems.bitmanipulation;
  * 
  * 
  */
-public class CheckKthBitSetOrNot {
+public class KthBitSetOrNot {
 	// Given a number N and a bit number K, check if Kth bit of N is set or not. A
 	// bit is called set if it is 1. Position of set bit '1' should be indexed
 	// starting with 0 from LSB side in binary representation of the number.
@@ -19,14 +19,37 @@ public class CheckKthBitSetOrNot {
 	// So, return true.
 	public static void main(String[] args) {
 		type1();
+		type2();
 	}
 
+	// rather than changing the input
+	// we can change the 1 and create one mask
+	// mask = 1<<k => 000100
+	// n = 011000
+	// n & mask => either 000000 or the 000100
+	// n & mask => 0 or mask
+	// if it is 0 that means the bit is not set
+	private static void type2() {
+		int n = 23;
+		int k = 3;
+		int mask = 1 << k;
+		int setBit = (n & mask) == 0 ? 0 : 1;
+		System.out.println(setBit);
+	}
+
+	// here we will right shift by k
+	// so when n = 11011100 and k = 3
+	// so n>>k = 00011011
+	// 00011011 & 1 => 1
+	// 1 means 00000001
+	// 00011011 & 00000001 => 1
+	// time complexity O(1)
+	// space complexity O(1)
 	private static void type1() {
-		int n = 4;
-		int k = 2;
+		int n = 23;
+		int k = 3;
 		int setBit = (n >> k) & 1;
 		System.out.println(setBit);
-
 	}
 
 }
--- src/problems/bitmanipulation/OddOrEven.java
+++ src/problems/bitmanipulation/OddOrEven.java
@@ -9,11 +9,16 @@ package problems.bitmanipulation;
  * 
  */
 public class OddOrEven {
-	//Given a positive integer N, determine whether it is odd or even.
+	// Given a positive integer N, determine whether it is odd or even.
 	public static void main(String[] args) {
 		type1();
 	}
 
+	// suppose n = 1101 in binary
+	// so we & with 1 then
+	// 1101 & 1 => 1101 & 0001 => 1
+	// if it is 1 then it is odd else even
+	// 1 means all bits are zero only the right most bit is 1
 	private static void type1() {
 		int n = 78;
 		int lsb = n & 1;
