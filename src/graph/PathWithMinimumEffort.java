26882f4dbc962477c8f082f29014929836727bb2
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2023-02-04 09:00:32 +0530

    commit

---
+++ src/graph/PathWithMinimumEffort.java
@@ -0,0 +1,28 @@
+package graph;
+
+/*
+ * Problem link :
+ * https://leetcode.com/problems/path-with-minimum-effort/
+ * https://practice.geeksforgeeks.org/problems/path-with-minimum-effort/1
+ * 
+ * Solution link :
+ * https://www.youtube.com/watch?v=0ytpZyiZFhA&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=39
+ * 
+ * https://takeuforward.org/data-structure/g-37-path-with-minimum-effort/
+ * 
+ */
+public class PathWithMinimumEffort {
+
+	public static void main(String[] args) {
+		type1();
+	}
+
+	private static void type1() {
+		int[][] heights = { { 1, 2, 2 }, { 3, 8, 2 }, { 5, 3, 5 } };
+		int rows=heights.length;
+		int column= heights[0].length;
+		int[][] minDis= new int[rows][column];
+		
+	}
+
+}
