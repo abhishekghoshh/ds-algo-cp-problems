64cd6f8fab5a7428b4128db46dd9b0b3c3d33f92
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2024-01-11 19:32:30 +0530

    commit

--- src/com/ds/stack/AsteroidCollision.java
+++ src/com/ds/stack/AsteroidCollision.java
@@ -18,6 +18,39 @@ public class AsteroidCollision {
         type1();
         type2();
         type3();
+        type4();
+    }
+
+    // it could be simplified more
+    // but it is ok
+    // it can be done anytime
+
+    // better approach
+    // Explains it in the interview
+    private static void type4() {
+        int[] asteroids = {10, 2, -5};
+        Stack<Integer> stack = new Stack<>();
+        // intuition is only a left side positive asteroid can collide with current negative asteroid
+        // because positive will go left and negative will go right
+        // and thats how they will collide
+        for (int asteroid : asteroids) {
+            if (stack.isEmpty() || stack.peek() < 0 || asteroid > 0) stack.push(asteroid);
+            else {
+                boolean isCurrentDestroyed = false;
+                while (!stack.isEmpty() && stack.peek() > 0) {
+                    int last = stack.pop();
+                    if (last >= -asteroid) {
+                        isCurrentDestroyed = true;
+                        if (last > -asteroid) stack.push(last);
+                        break;
+                    }
+                }
+                if (!isCurrentDestroyed) stack.push(asteroid);
+            }
+        }
+        int[] answer = new int[stack.size()];
+        for (int i = answer.length - 1; i >= 0; i--) answer[i] = stack.pop();
+        ArrayUtil.print(answer);
     }
 
     // same as type2,
@@ -31,6 +64,7 @@ public class AsteroidCollision {
         int last, current;
         for (int asteroid : asteroids) {
             stack[++top] = asteroid;
+            // at least there are two asteroids
             while (top >= 1) {
                 current = stack[top];
                 last = stack[top - 1];
@@ -49,8 +83,9 @@ public class AsteroidCollision {
     // optimized approach
     // using stack,
     // we will add the asteroids one by one to the stack
-    // and pop the top two items and check if they will collide or not
-
+    // and pop the top two items and check if they will collide or not,
+    // we can only destroy the asteroids if there are at least 2 asteroids,
+    // and depending on the top 2 asteroids weight and sign, we either push one or none
     private static void type2() {
         int[] asteroids = {10, 2, -5};
         Stack<Integer> stack = new Stack<>();
