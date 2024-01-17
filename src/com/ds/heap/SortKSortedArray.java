59a2e60bd29886d5fdfdb2ba8271d94f7b09671e
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2024-01-17 10:17:06 +0530

    commit

--- src/com/ds/heap/SortKSortedArray.java
+++ src/com/ds/heap/SortKSortedArray.java
@@ -3,6 +3,7 @@ package com.ds.heap;
 import com.util.ArrayUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 /*
  * Problem link :
@@ -14,32 +15,52 @@ import java.util.PriorityQueue;
  * https://www.geeksforgeeks.org/nearly-sorted-algorithm/
  * */
 public class SortKSortedArray {
+
+	// Given an array of N elements, where each element is
+	// at most K away from its target position,
+	// devise an algorithm that sorts in O(N log K) time.
 	public static void main(String[] args) {
 		type1();
 		type2();
 	}
 
+	// time complexity  O(NlogK)
 	private static void type2() {
 		int[] nums = {6, 5, 3, 2, 8, 10, 9};
 		int k = 3;
 		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 		ArrayList<Integer> answer = new ArrayList<>();
 		for (int num : nums) {
+			// at means after k+1 times of adding,
+			// we will get the item
+			// like for the series 6 5 3 2
+			// for getting 0th element we have to add k+1
+			// element in the heap
 			if (minHeap.size() <= k) {
+				// the max size of heap can be k+1
 				minHeap.offer(num);
 			} else {
 				answer.add(minHeap.poll());
 				minHeap.offer(num);
 			}
 		}
-		while (answer.size() < nums.length) answer.add(minHeap.poll());
+		// after all the iteration, there should be some elements
+		// in the heap; we will add them one by one to the answer
+		while (!minHeap.isEmpty()) answer.add(minHeap.poll());
 
-		ArrayUtil.print(nums);
+		ArrayUtil.print(answer);
 	}
 
 	// Brute force approach
 	public static void type1() {
+		int[] nums = {6, 5, 3, 2, 8, 10, 9};
+		int k = 3;
+		ArrayList<Integer> answer = new ArrayList<>();
+		for (int num : nums)
+			answer.add(num);
+		Collections.sort(answer);
 
+		ArrayUtil.print(answer);
 	}
 
 }
