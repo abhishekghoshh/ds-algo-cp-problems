215144c6004ccbaeec8ae145c7a9b5b961c88dc1
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2024-01-23 19:18:41 +0530

    commit

--- src/com/ds/linkedlist/SegregateOddAndEvenNodesInLinkedList.java
+++ src/com/ds/linkedlist/SegregateOddAndEvenNodesInLinkedList.java
@@ -1,5 +1,12 @@
 package com.ds.linkedlist;
 
+import com.algo.linkedlist.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.util.ArrayUtil.print;
+
 /*
  *
  * problem links :
@@ -22,10 +29,53 @@ public class SegregateOddAndEvenNodesInLinkedList {
     }
 
     private static void type2() {
-
+        Node head = new Node(2, 1, 3, 5, 6, 4, 7);
+        print(head);
+        head = oddEvenList(head);
+        print(head);
     }
 
-    private static void type1() {
+    // in place link construction
+    public static Node oddEvenList(Node head) {
+        if (head == null || head.next == null) return head;
+        Node odd = head;
+        Node even = head.next;
+        Node enHead = even;
+        while (even != null && even.next != null && odd != null && odd.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = enHead;
+        return head;
+    }
 
+    // brute force approach
+    // we will add all the node data to a list
+    private static void type1() {
+        Node head = new Node(2, 1, 3, 5, 6, 4, 7);
+        print(head);
+        Node node = head;
+        // adding all the node data in the array list
+        List<Integer> list = new ArrayList<>();
+        while (null != node) {
+            list.add(node.data);
+            node = node.next;
+        }
+        int n = list.size();
+        node = head;
+        // now we will add data's back to the linked list
+        // first we will assign the even index nodes
+        for (int i = 0; i < n && node != null; i += 2) {
+            node.data = list.get(i);
+            node = node.next;
+        }
+        // then we will assign the odd index nodes
+        for (int i = 1; i < n && node != null; i += 2) {
+            node.data = list.get(i);
+            node = node.next;
+        }
+        print(head);
     }
 }
