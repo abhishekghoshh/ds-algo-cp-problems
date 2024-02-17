0cc867b1e36dd7a6ddf0b20fa1c0b90475877a17
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2024-02-17 11:41:20 +0530

    commit

--- src/graph/BipartiteGraph.java
+++ src/graph/BipartiteGraph.java
@@ -6,24 +6,35 @@ import java.util.Queue;
 /*
  * Problem link :
  * https://leetcode.com/problems/is-graph-bipartite/
- * https://practice.geeksforgeeks.org/problems/bipartite-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bipartite-graph
- * 
+ * https://practice.geeksforgeeks.org/problems/bipartite-graph/1
+ * https://www.codingninjas.com/studio/problems/check-bipartite-graph-_920551
  * 
  * Solution link :
- * https://www.youtube.com/watch?v=7zmgQSJghpo&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=17
  * https://www.youtube.com/watch?v=-vu34sct1g8&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=17
+ * https://www.youtube.com/watch?v=KG5YFfR0j8A&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=18
  * 
  * https://takeuforward.org/graph/bipartite-graph-dfs-implementation/
+ * https://takeuforward.org/graph/bipartite-graph-bfs-implementation/
  */
 
 public class BipartiteGraph {
-	// A graph is bipartite if the nodes can be partitioned into two independent
-	// sets A and B such that every edge in the graph connects a node in set A and a
-	// node in set B.
 
-	// it is also an graph coloring problem
-	// the problem is here if we can color the graph with 2 color only such that
-	// no 2 adjacent vertex has the same color
+	/*
+	 * There is an undirected graph with n nodes, where each node is numbered between 0 and n - 1.
+	 * You are given a 2D array graph, where graph[u] is an array of nodes that node u is adjacent to.
+	 * More formally, for each v in graph[u], there is an undirected edge between node u and node v.
+	 *
+	 * The graph has the following properties:
+	 * 1. There are no self-edges (graph[u] does not contain u).
+	 * 2. There are no parallel edges (graph[u] does not contain duplicate values).
+	 * 3. If v is in graph[u], then u is in graph[v] (the graph is undirected).
+	 * 4. The graph may not be connected, meaning there may be two nodes u and v such that there is no path between them.
+	 *
+	 * A graph is bipartite if the nodes can be partitioned into two independent sets A and B
+	 * such that every edge in the graph connects a node in set A and a node in set B.
+	 *
+	 * Return true if and only if it is bipartite.
+	 * */
 	public static void main(String[] args) {
 		type1();
 		type2();
@@ -31,30 +42,35 @@ public class BipartiteGraph {
 
 	// using DFS
 	private static void type2() {
-		int[][] adjacencyList = { { 1, 2, 3 }, { 0, 2 }, { 0, 1, 3 }, { 0, 2 } };
-		int v = adjacencyList.length;
-		int[] colors = new int[v];
-		boolean isBipatite = true;
-		// start from all components
-		for (int i = 0; i < v; i++) {
-			if (colors[i] == 0 && !dfs(adjacencyList, colors, i, 1)) {
-				isBipatite = false;
-				break;
-			}
-		}
-		System.out.println(isBipatite);
+		int[][] adjacencyList = {
+				{1, 2, 3},
+				{0, 2},
+				{0, 1, 3},
+				{0, 2}
+		};
+		boolean isBipartite = isBipartite2(adjacencyList);
+		System.out.println(isBipartite);
 	}
 
-	private static boolean dfs(int[][] adjacencyList, int[] colors, int i, int color) {
-		colors[i] = color;
+	private static boolean isBipartite2(int[][] adjacencyList) {
+		int v = adjacencyList.length;
+		int[] colors = new int[v];
+		// start from all components
+		for (int i = 0; i < v; i++)
+			if (colors[i] == 0 && !canColor2(adjacencyList, colors, i, 1))
+				return false;
+		return true;
+	}
+
+	private static boolean canColor2(int[][] adjacencyList, int[] colors, int node, int color) {
+		colors[node] = color;
 		// adjacent node
-		for (int k : adjacencyList[i]) {
-			// adjacent node color is opposite so we don't have to do anything
-			if (colors[k] == -color)
-				continue;
-			// if the adjacent color is same then we will directly return false
+		for (int end : adjacencyList[node]) {
+			// adjacent node color is opposite, so we don't have to do anything
+			if (colors[end] == -color) continue;
+			// if the adjacent color is the same, then we will directly return false
 			// else we will start dfs from the adjacent node with the opposite color
-			if (colors[k] == color || !dfs(adjacencyList, colors, k, -color))
+			if (colors[end] == color || !canColor2(adjacencyList, colors, end, -color))
 				return false;
 		}
 		return true;
@@ -62,39 +78,45 @@ public class BipartiteGraph {
 
 	// using BFS
 	private static void type1() {
-		int[][] adjacencyList = { { 1, 2, 3 }, { 0, 2 }, { 0, 1, 3 }, { 0, 2 } };
-		int v = adjacencyList.length;
-
-		int[] color = new int[v];
-		boolean isBipatite = true;
-		// start from all components
-		for (int i = 0; i < v; i++) {
-			if (color[i] == 0 && !isBipatite(i, adjacencyList, color)) {
-				isBipatite = false;
-				break;
-			}
-		}
-		System.out.println(isBipatite);
+		int[][] adjacencyList = {
+				{1, 2, 3},
+				{0, 2},
+				{0, 1, 3},
+				{0, 2}
+		};
+		boolean isBipartite = isBipartite1(adjacencyList);
+		System.out.println(isBipartite);
 	}
 
-	private static boolean isBipatite(int i, int[][] adjacencyList, int[] color) {
-		color[i] = 1;
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(i);
+	private static boolean isBipartite1(int[][] adjacencyList) {
+		int v = adjacencyList.length;
+		int[] colors = new int[v];
+		// start from all components
+		for (int i = 0; i < v; i++)
+			if (colors[i] == 0 && !canColor1(i, adjacencyList, colors))
+				return false;
+		return true;
+	}
 
+	// this is using BFS
+	private static boolean canColor1(int node, int[][] adjacencyList, int[] colors) {
+		// we will initialize the node color with 1, and add that node to the queue
+		colors[node] = 1;
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(node);
+		// we will start bfs from the node
 		while (!queue.isEmpty()) {
-			int pt = queue.poll();
-			int cpt = color[pt];
-			for (int vertex : adjacencyList[pt]) {
-				// if adjacent node is not colored
-				// then we will add opposite color to the adjacent node
-				// and add that to queue
-				if (color[vertex] == -1) {
-					queue.offer(vertex);
-					color[vertex] = -cpt;
-				} else if (color[vertex] == cpt) {
-					// if the color is same in adjacent node then we will return false
-					return false;
+			int start = queue.poll();
+			//
+			int color = colors[start];
+			for (int end : adjacencyList[start]) {
+				// if the color is same in adjacent node, then we will return false
+				if (colors[end] == color) return false;
+				// if the adjacent node is not colored, then we will add
+				// opposite color to the adjacent node and add that to queue
+				if (colors[end] == 0) {
+					queue.offer(end);
+					colors[end] = -color;
 				}
 			}
 		}
--- src/graph/CycleDetectionInDirectedGraph.java
+++ src/graph/CycleDetectionInDirectedGraph.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 /*
  * Problem link :
+ * https://leetcode.com/problems/course-schedule-ii/description/
  * https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
  * https://leetcode.com/problems/course-schedule-ii/solutions/293048/detecting-cycle-in-directed-graph-problem/
+ * https://www.codingninjas.com/studio/problems/detect-cycle-in-a-directed-graph-_920545
  * 
  * Solution link :
  * https://www.youtube.com/watch?v=9twcmtQj4DU&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=19
@@ -21,40 +23,36 @@ public class CycleDetectionInDirectedGraph {
 		type2();
 	}
 
-	// single visited array instead of two array
+	// single visited array instead of two arrays
 	private static void type2() {
 		int v = 10;
 		ArrayList<ArrayList<Integer>> adjacencyList = adjacencyList();
+		boolean hasCycle = isCyclic2(v, adjacencyList);
+		System.out.println(hasCycle);
+	}
 
-		// we will use 0 as not visited
-		// 1 as visited and 2 as path visited
+	private static boolean isCyclic2(int v, ArrayList<ArrayList<Integer>> adjacencyList) {
+		// we will use 0 as not visited, 1 as visited and 2 as path visited
 		int[] visited = new int[v];
-
-		boolean hasCycle = false;
 		for (int i = 0; i < v; i++) {
 			// check for all the component
-			if (visited[i] == 0 && hasCycle(i, visited, adjacencyList)) {
-				hasCycle = true;
-				break;
-			}
+			if (visited[i] == 0 && hasCycle(i, visited, adjacencyList))
+				return true;
 		}
-		System.out.println(hasCycle);
+		return false;
 	}
 
 	private static boolean hasCycle(int start, int[] visited, ArrayList<ArrayList<Integer>> adjacencyList) {
 		// visited and path visited
 		visited[start] = 2;
-		for (int node : adjacencyList.get(start)) {
-			if (visited[node] == 0) {
-				if (hasCycle(node, visited, adjacencyList)) {
-					return true;
-				}
-			} else if (visited[node] == 2) {
+		for (int end : adjacencyList.get(start)) {
+			if (visited[end] == 0 && hasCycle(end, visited, adjacencyList)) {
+				return true;
+			} else if (visited[end] == 2) {
 				return true;
 			}
 		}
-		// resetting the path visited
-		// setting only visited
+		// resetting the path visited setting only visited
 		visited[start] = 1;
 		return false;
 	}
@@ -62,27 +60,44 @@ public class CycleDetectionInDirectedGraph {
 	private static void type1() {
 		int v = 10;
 		ArrayList<ArrayList<Integer>> adjacencyList = adjacencyList();
+		boolean hasCycle = isCyclic1(v, adjacencyList);
+		System.out.println(hasCycle);
+	}
 
+	private static boolean isCyclic1(int v, ArrayList<ArrayList<Integer>> adjacencyList) {
 		boolean[] visited = new boolean[v];
 		// path visited is to track the nodes in a single path
-		// because visited array will not be suffice
-		// if 1 points to 2 and 3
-		// 2 and 3 both points to 4
+		// because visited array will not be sufficed
+		// if 1 points to 2 and 3 , 2 and 3 both points to 4
 		// so 1 to 4 will from a cycle if we consider the visited array only
 		// and we will choose dfs here because we want to as deep as possible and in
-		// this path if we come to any of previous visited point then that will form a
-		// cycle,
+		// this path if we come to any of the previous visited point then that will form a cycle,
 		boolean[] pathVisited = new boolean[v];
-
-		boolean hasCycle = false;
 		for (int i = 0; i < v; i++) {
 			// check for all the component
-			if (!visited[i] && hasCycle(i, visited, pathVisited, adjacencyList)) {
-				hasCycle = true;
-				break;
-			}
+			if (!visited[i] && hasCycle(i, visited, pathVisited, adjacencyList))
+				return true;
 		}
-		System.out.println(hasCycle);
+		return false;
+	}
+
+	private static boolean hasCycle(int start, boolean[] visited, boolean[] pathVisited,
+									ArrayList<ArrayList<Integer>> adjacencyList) {
+		// at the start, we set visited and path visited to true
+		visited[start] = true;
+		pathVisited[start] = true;
+		// traverse for adjacent nodes
+		for (int end : adjacencyList.get(start)) {
+			// if the node has been previously visited, but it has to be visited on the same path
+			if (pathVisited[end]) return true;
+			// when the node is not visited, then we call dfs from that node
+			if (!visited[end] && hasCycle(end, visited, pathVisited, adjacencyList))
+				return true;
+		}
+		// before returning false, we are setting a path visited to false as dfs call is
+		// completed so the path from this node is traversed and no cycle found
+		pathVisited[start] = false;
+		return false;
 	}
 
 	private static ArrayList<ArrayList<Integer>> adjacencyList() {
@@ -100,30 +115,5 @@ public class CycleDetectionInDirectedGraph {
 		return adjacencyList;
 	}
 
-	private static boolean hasCycle(int start, boolean[] visited, boolean[] pathVisited,
-			ArrayList<ArrayList<Integer>> adjacencyList) {
-		// at the start we set visited and path visited to true
-		visited[start] = true;
-		pathVisited[start] = true;
-
-		// traverse for adjacent nodes
-		for (int node : adjacencyList.get(start)) {
-			// when the node is not visited
-			// then we call dfs from that node
-			if (!visited[node]) {
-				if (hasCycle(node, visited, pathVisited, adjacencyList))
-					return true;
-			}
-			// if the node has been previously visited
-			// but it has to be visited on the same path
-			else if (pathVisited[node]) {
-				return true;
-			}
-		}
-		// before returning false we are setting path visited to false as dfs call is
-		// completed so path from this node is traversed and no cycle found
-		pathVisited[start] = false;
-		return false;
-	}
 
 }
