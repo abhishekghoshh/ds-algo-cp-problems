3c8aacdbe62556631906d32f5d0ff42a40397796
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2024-04-27 19:57:05 +0530

    commit

--- src/com/ds/recursion/KthPermutationSequence.java
+++ src/com/ds/recursion/KthPermutationSequence.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class KthPermutationSequence {
 
 
+	// TODO check the solution again
 	public static void main(String[] args) {
 		type1();
 		type2();
@@ -24,27 +25,31 @@ public class KthPermutationSequence {
 	// iterative way
 	private static void type3() {
 		int n = 4;
-		int k = 1;
+		int k = 19;
 
 		// 0 to n - 1 factorial
 
 		ArrayList<Integer> nums = new ArrayList<>();
 		nums.add(1);
 
-		int[] factorial = new int[n];
-		factorial[0] = 1;
+		int[] fact = new int[n];
+		fact[0] = 1;
 
 		for (int i = 1; i < n; i++) {
 			nums.add(i + 1);
-			factorial[i] = factorial[i - 1] * i;
+			fact[i] = fact[i - 1] * i;
 		}
 
 		StringBuilder result = new StringBuilder();
 		int pos;
 		for (int i = n - 1; i >= 0; i--) {
-			pos = (k - 1) / factorial[i];
+			// getting the position
+			pos = (k - 1) / fact[i];
+			// appending the num to the result
 			result.append(nums.get(pos));
-			k = k - (pos * factorial[i]);
+			// now updating the k again
+			k = k - (pos * fact[i]);
+			// we will remove the num from the num array
 			nums.remove(pos);
 		}
 		System.out.println(result);
@@ -58,36 +63,36 @@ public class KthPermutationSequence {
 	// second 2 numbers starts with, this we can find the remaining
 	private static void type2() {
 		int n = 4;
-		int k = 1;
+		int k = 19;
 		List<Integer> numbers = new ArrayList<>();
-		int factorial = 1;
+		int fact = 1;
 		for (int i = 1; i <= n; i++) {
 			numbers.add(i);
-			factorial = factorial * i;
+			fact = fact * i;
 		}
 		StringBuilder answer = new StringBuilder();
 		// as the group will be formed in 0 indexing formats
 		// like 0 to n-1 so subtracting k to 1
-		computeKthPermutation(numbers, factorial, answer, k - 1);
+		computeKthPermutation(numbers, fact, answer, k - 1);
 		System.out.println(answer);
 	}
 
-	private static void computeKthPermutation(List<Integer> numbers, int factorial, StringBuilder answer, int k) {
-		if (numbers.isEmpty()) return;
-		// currently factorial is n! where n is the size of numbers
-		// so there will be n groups of (n-1)! numbers
-		// if we divide it by numbers.size() then we will find (n-1)!
-		factorial = factorial / numbers.size();
+	private static void computeKthPermutation(List<Integer> num, int fact, StringBuilder sb, int k) {
+		if (num.isEmpty()) return;
+		// currently factorial is n!
+		// where n is the size of numbers, so there will be n groups of (n-1)!
+		// numbers, if we divide it by numbers.size() then we will find (n-1)!
+		fact = fact / num.size();
 		// now we will find in which group it will fall
-		int position = k / factorial;
-		answer.append(numbers.get(position));
+		int pos = k / fact;
+		sb.append(num.get(pos));
 		// once we get that number, we will remove that from numbers
 		// let our number is 1234 and the position is 2, so we will take 3 and remove it,
 		// so we will have 124
-		numbers.remove(position);
+		num.remove(pos);
 		// now we will find our next group
-		k = k % factorial;
-		computeKthPermutation(numbers, factorial, answer, k);
+		k = k % fact;
+		computeKthPermutation(num, fact, sb, k);
 	}
 
 	// TODO first explain this approach
