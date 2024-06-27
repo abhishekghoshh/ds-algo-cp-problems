3e584da455800b2e0e17a97c77bfee597438aa10
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2024-06-27 12:17:00 +0530

    commit

--- src/com/problems/dp/LargestDivisibleSubset.java
+++ src/com/problems/dp/LargestDivisibleSubset.java
@@ -2,10 +2,11 @@ package com.problems.dp;
 
 /*
  * Problem link :
- *
+ * https://leetcode.com/problems/largest-divisible-subset/description/
+ * https://www.naukri.com/code360/problems/divisible-set_3754960
  *
  * Solution link :
- *
+ * https://www.youtube.com/watch?v=gDuZwBW9VvM&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=45
  *
  */
 public class LargestDivisibleSubset {
